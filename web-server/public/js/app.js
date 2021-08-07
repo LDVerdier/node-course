56a11f52556d0528b33ad0159ffc2ab688b856e0
laurent <ldverdier@gmail.com>
2021-08-07 22:38:59 +0200

    weather app completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,34 @@
-console.log('Hey !');
+// console.log('Hey !');
+
+
+// const url = 'http://puzzle.mead.io/puzzle'
+
+
+// .catch((error) => {console.log(error)})
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const search = document.querySelector('input')
+const weatherForm = document.querySelector('form')
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = search.value 
+    if (location) {
+        messageOne.textContent = 'loading...'
+        messageTwo.textContent = ''
+        const url = 'http://localhost:3000/weather?address=' + location
+        fetch(url)
+        .then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                    return console.log(data.error)
+                }
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                console.log(data.forecast, data.location)
+            })
+        })
+    }
+})
+
